60ba207f63487e875b21ccaceca83b87db3d99a9
szepnapot <silentpocok@gmail.com>
2017-10-07 10:29:54 +0200

    refactor

--- player.py
+++ player.py
@@ -6,22 +6,26 @@ class Player:
         player = [elem['hole_cards'] for elem in game_state['players'] if elem['version'] == self.VERSION]
         return player
 
+
     def get_community_card(self, game_state):
         return game_state["community_cards"]
+
+    def printer(self, *args):
+        separator = lambda: print("#" * 50)
+        separator()
+        print("                 OUR HAND                       ")
+        print(args[0])
+        separator()
+        separator()
+        print("                 COMMUNITY CARDS                ")
+        print(args[1])
+        separator()
+        separator()
 
     def betRequest(self, game_state):
         hand = self.get_hand(game_state)
         community_cards = self.get_community_card(game_state)
-        print("###############################################")
-        print("###############################################")
-        print("                 OUR HAND                       ")
-        print(hand)
-        print("###############################################")
-        print("###############################################")
-        print("                 COMMUNITY CARDS                ")
-        print("Community cards: {}".format(community_cards))
-        print("###############################################")
-        print("###############################################")
+        self.printer(hand, community_cards)
         return 9999
 
     def showdown(self, game_state):
