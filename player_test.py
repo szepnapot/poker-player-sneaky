54293eede60f3d70927dd5b1048f41ca47f39a37
szepnapot <silentpocok@gmail.com>
2017-10-07 14:12:02 +0200

    tests, showdown winner show

--- player_test.py
+++ player_test.py
@@ -1,8 +1,9 @@
 from player import Player
-from test_datas import test_game_state
+from test_datas import test_game_state, test_showdown
 
 pl = Player()
 
-assert pl.betRequest(test_game_state) == 9999
+assert pl.betRequest(test_game_state) == 99999
 
 
+print(pl.showdown(test_showdown))
