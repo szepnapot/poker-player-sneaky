2d805b5080291aed17a51889190b0b4782c84c55
szepnapot <silentpocok@gmail.com>
2017-10-07 10:19:59 +0200

    added logging

--- player.py
+++ player.py
@@ -2,9 +2,22 @@
 class Player:
     VERSION = "Default Python folding player"
 
+    def get_player(self, game_state):
+        player = [elem['hole_cards'] for elem in game_state['players'] if elem['version'] == self.VERSION]
+        return player
+
     def betRequest(self, game_state):
+        print("###############################################")
+        print("###############################################")
+        print("                 OUR HAND                       ")
+        print(self.get_player(game_state))
+        print("###############################################")
+        print("###############################################")
+        print("                 COMMUNITY CARDS                ")
         print("Community cards: {}".format(game_state["community_cards"]))
-        return 9999
+        print("###############################################")
+        print("###############################################")
+        return 99999
 
     def showdown(self, game_state):
         pass
