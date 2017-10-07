a8f27a351f4e6444a7c0f2cba99544926af832ad
szepnapot <silentpocok@gmail.com>
2017-10-07 10:35:07 +0200

    revert printer

--- player.py
+++ player.py
@@ -6,7 +6,6 @@ class Player:
         player = [elem['hole_cards'] for elem in game_state['players'] if elem['version'] == self.VERSION]
         return player
 
-
     def get_community_card(self, game_state):
         return game_state["community_cards"]
 
@@ -24,7 +23,15 @@ class Player:
     def betRequest(self, game_state):
         hand = self.get_hand(game_state)
         community_cards = self.get_community_card(game_state)
-        self.printer(hand, community_cards)
+        print("#######################################")
+        print("                 OUR HAND                       ")
+        print(hand)
+        print("#######################################")
+        print("#######################################")
+        print("                 COMMUNITY CARDS                ")
+        print(community_cards)
+        print("#######################################")
+        print("#######################################")
         return 9999
 
     def showdown(self, game_state):
